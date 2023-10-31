66a6576d05b652efa1c358b03454061eb8661a66
adamcq <adamborgula@gmail.com>
2023-10-31 20:57:00 +0100

    changed base url for backend

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -4,7 +4,7 @@ const UserImage = ({ image, size="60px" }) => {
   return (
     <Box width={size} height={size}>
       <img 
-        src={`http://localhost:3001/assets/${image}`}
+        src={`https://technotes-api.onrender.com/assets/${image}`}
         alt="user" 
         style={{ objectFit: "cover", borderRadius: "50%" }}
         width={ size }
